5acd7fb56782d13e0022d9932e7f21093b055c09
Ivan Ermacoff <jermacoff@gmail.com>
2022-04-14 14:03:36 +0700

    Update Unity

--- Assets/SpaceSmuggler/Scripts/Weapons/Bullet.cs
+++ Assets/SpaceSmuggler/Scripts/Weapons/Bullet.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        Destroy(gameObject);
-    }
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		Destroy(gameObject);
+	}
 }
