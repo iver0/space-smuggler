56d06f23762729ffcefb5dbeb65a20bbb293d5bb
Ivan Ermacoff <jermacoff@gmail.com>
2022-05-10 14:40:51 +0700

    Add player hurt SFX

--- Assets/SpaceSmuggler/Scripts/Audio/AudioManagerSO.cs
+++ Assets/SpaceSmuggler/Scripts/Audio/AudioManagerSO.cs
@@ -3,41 +3,40 @@ using Random = UnityEngine.Random;
 
 namespace SpaceSmuggler
 {
-	/// <summary>
-	/// Project audio management.
-	/// </summary>
-	[CreateAssetMenu(fileName = "AudioManager", menuName = "Gameplay/Audio Manager")]
-	public class AudioManagerSO : ScriptableObject
-	{
-		[Header("Listening on")]
-		[SerializeField] AudioCueEventChannelSO _SFXEventChannel = default;
-		[HideInInspector] public AudioSource Source;
+    /// <summary>
+    /// Project audio management.
+    /// </summary>
+    [CreateAssetMenu(fileName = "AudioManager", menuName = "Gameplay/Audio Manager")]
+    public class AudioManagerSO : ScriptableObject
+    {
+        [Header("Listening on")]
+        [SerializeField] AudioCueEventChannelSO _SFXEventChannel = default;
+        [HideInInspector] public AudioSource Source;
 
-		void OnEnable()
-		{
-			_SFXEventChannel.OnAudioCueRequested += Play;
+        void OnEnable()
+        {
+            _SFXEventChannel.OnAudioCueRequested += Play;
+        }
 
-		}
+        void OnDisable()
+        {
+            _SFXEventChannel.OnAudioCueRequested -= Play;
+        }
 
-		void OnDisable()
-		{
-			_SFXEventChannel.OnAudioCueRequested -= Play;
-		}
-
-		/// <summary>
-		/// Plays audio with the specified configuration.
-		/// </summary>
-		public void Play(AudioEventSO audioEvent)
-		{
-			AudioSource source;
-			if (audioEvent.Source != null)
-				source = audioEvent.Source;
-			else
-				source = Source;
-			source.clip = audioEvent.Clips[Random.Range(0, audioEvent.Clips.Length)];
-			source.volume = Random.Range(audioEvent.Volume.minValue, audioEvent.Volume.maxValue);
-			source.pitch = Random.Range(audioEvent.Pitch.minValue, audioEvent.Pitch.maxValue);
-			source.Play();
-		}
-	}
+        /// <summary>
+        /// Plays audio with the specified configuration.
+        /// </summary>
+        public void Play(AudioEventSO audioEvent)
+        {
+            AudioSource source;
+            if (audioEvent.Source != null)
+                source = audioEvent.Source;
+            else
+                source = Source;
+            source.clip = audioEvent.Clips[Random.Range(0, audioEvent.Clips.Length)];
+            source.volume = Random.Range(audioEvent.Volume.minValue, audioEvent.Volume.maxValue);
+            source.pitch = Random.Range(audioEvent.Pitch.minValue, audioEvent.Pitch.maxValue);
+            source.Play();
+        }
+    }
 }
--- Assets/SpaceSmuggler/Scripts/Characters/Player.cs
+++ Assets/SpaceSmuggler/Scripts/Characters/Player.cs
@@ -3,102 +3,104 @@ using UnityEngine;
 
 namespace SpaceSmuggler
 {
-	/// <summary>
-	/// Player base class.
-	/// </summary>
-	public class Player : MonoBehaviour
-	{
-		#region Fields
-		[SerializeField] PlayerHealthSO Health = default;
-		//[SerializeField] PlayerArmorSO Armor = default;
-		[SerializeField] InputReaderSO _inputReader = default;
-		[SerializeField] PlayerDataSO _playerData = default;
-		[Header("Listening on")]
-		[SerializeField] ItemEventChannelSO _healthItemEvent = default;
-		Rigidbody2D _rb;
-		Vector2 _look;
-		Vector2 _moveDirection;
-		Vector2 _mousePosition;
-		#endregion
+    /// <summary>
+    /// Player base class.
+    /// </summary>
+    public class Player : MonoBehaviour
+    {
+        #region Fields
+        [SerializeField] PlayerHealthSO Health = default;
+        //[SerializeField] PlayerArmorSO Armor = default;
+        [SerializeField] InputReaderSO _inputReader = default;
+        [SerializeField] PlayerDataSO _playerData = default;
+        [SerializeField] AudioEventSO _playerHurtSFX;
+        [Header("Listening on")]
+        [SerializeField] ItemEventChannelSO _healthItemEvent = default;
+        Rigidbody2D _rb;
+        Vector2 _look;
+        Vector2 _moveDirection;
+        Vector2 _mousePosition;
+        #endregion
 
-		#region LifeCycle
-		void Awake()
-		{
-			_rb = GetComponent<Rigidbody2D>();
-		}
+        #region LifeCycle
+        void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
 
-		void OnEnable()
-		{
-			_healthItemEvent.OnEventRaised += OnCollect;
-			_inputReader.MoveEvent += OnMove;
-			_inputReader.LookEvent += OnLook;
-		}
+        void OnEnable()
+        {
+            _healthItemEvent.OnEventRaised += OnCollect;
+            _inputReader.MoveEvent += OnMove;
+            _inputReader.LookEvent += OnLook;
+        }
 
-		void OnDisable()
-		{
-			_healthItemEvent.OnEventRaised -= OnCollect;
-			_inputReader.MoveEvent -= OnMove;
-			_inputReader.LookEvent -= OnLook;
-		}
+        void OnDisable()
+        {
+            _healthItemEvent.OnEventRaised -= OnCollect;
+            _inputReader.MoveEvent -= OnMove;
+            _inputReader.LookEvent -= OnLook;
+        }
 
-		void Update()
-		{
-			_mousePosition = Camera.main.ScreenToWorldPoint(_look);
-		}
+        void Update()
+        {
+            _mousePosition = Camera.main.ScreenToWorldPoint(_look);
+        }
 
-		void FixedUpdate()
-		{
-			_rb.velocity = new Vector2(
-				_moveDirection.x * _playerData.MoveSpeed,
-				_moveDirection.y * _playerData.MoveSpeed
-			);
+        void FixedUpdate()
+        {
+            _rb.velocity = new Vector2(
+                _moveDirection.x * _playerData.MoveSpeed,
+                _moveDirection.y * _playerData.MoveSpeed
+            );
 
-			Vector2 aimDirection = _mousePosition - _rb.position;
-			float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
-			_rb.rotation = aimAngle;
-		}
-		#endregion
+            Vector2 aimDirection = _mousePosition - _rb.position;
+            float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+            _rb.rotation = aimAngle;
+        }
+        #endregion
 
-		#region PublicMethods
-		/// <summary>
-		/// Decrease player health by damage amount.
-		/// </summary>
-		public void TakeDamage(int damage)
-		{
-			_playerData.Health -= damage;
-		}
+        #region PublicMethods
+        /// <summary>
+        /// Decrease player health by damage amount.
+        /// </summary>
+        public void TakeDamage(int damage)
+        {
+            _playerHurtSFX.Play();
+            _playerData.Health -= damage;
+        }
 
-		IEnumerator FadeEffect(ItemSO item, int maxHealth)
-		{
-			yield return new WaitForSeconds(item.Duration);
-			int tmpIncrement = item.Increment;
-			int tmpNewMax = item.NewMax;
-			item.Increment = 0;
-			item.NewMax = maxHealth;
-			Health.OnCollect(item);
-			item.Increment = tmpIncrement;
-			item.NewMax = tmpNewMax;
-		}
-		#endregion
+        IEnumerator FadeEffect(ItemSO item, int maxHealth)
+        {
+            yield return new WaitForSeconds(item.Duration);
+            int tmpIncrement = item.Increment;
+            int tmpNewMax = item.NewMax;
+            item.Increment = 0;
+            item.NewMax = maxHealth;
+            Health.OnCollect(item);
+            item.Increment = tmpIncrement;
+            item.NewMax = tmpNewMax;
+        }
+        #endregion
 
-		#region EventListeners
-		void OnCollect(ItemSO item)
-		{
-			int tmp = _playerData.MaxHealth;
-			Health.OnCollect(item);
-			if (item.Duration > 0)
-				StartCoroutine(FadeEffect(item, tmp));
-		}
+        #region EventListeners
+        void OnCollect(ItemSO item)
+        {
+            int tmp = _playerData.MaxHealth;
+            Health.OnCollect(item);
+            if (item.Duration > 0)
+                StartCoroutine(FadeEffect(item, tmp));
+        }
 
-		void OnMove(Vector2 move)
-		{
-			_moveDirection = move;
-		}
+        void OnMove(Vector2 move)
+        {
+            _moveDirection = move;
+        }
 
-		void OnLook(Vector2 look)
-		{
-			_look = look;
-		}
-		#endregion
-	}
+        void OnLook(Vector2 look)
+        {
+            _look = look;
+        }
+        #endregion
+    }
 }
